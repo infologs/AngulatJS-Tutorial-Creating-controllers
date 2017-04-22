5668ee812c0ef818f58b79af41c0a039c4b85967
Infologs <webboostings@gmail.com>
2017-04-22 23:35:45 +0530

    Creating controllers in angularjs

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module("app01", []);
+
+app.controller("controller1", function () {
+    this.ControllerName = "Login Page Controller";
+});
+
+app.controller("controller2", function () {
+    this.ControllerName = "Registration Page Controller";
+});;
